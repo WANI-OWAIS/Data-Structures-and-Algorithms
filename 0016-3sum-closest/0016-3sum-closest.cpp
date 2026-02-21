b80bc22a20340e08b1ff7d37167f6642bc8aa3e5
OWAIS HASSAN WANI <142871919+WANI-OWAIS@users.noreply.github.com>
2026-02-21 16:24:57 +0530

    Time: 8 ms (75.55%), Space: 13.9 MB (64.04%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        int closestSum = nums[0] + nums[1] + nums[2];  // initial answer
+        
+        for(int i = 0; i < n - 2; i++){
+            
+            int left = i + 1;
+            int right = n - 1;
+            
+            while(left < right){
+                
+                int currentSum = nums[i] + nums[left] + nums[right];
+                
+                // Update closest sum if better
+                if(abs(currentSum - target) < abs(closestSum - target)){
+                    closestSum = currentSum;
+                }
+                
+                // Move pointers
+                if(currentSum < target){
+                    left++;
+                }
+                else if(currentSum > target){
+                    right--;
+                }
+                else{
+                    // Exact match found
+                    return currentSum;
+                }
+            }
+        }
+        
+        return closestSum;
+    }
+};
